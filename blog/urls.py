59471921ef15f3258ac72815db6e0d2f71834c49
Leandro Ezequiel Soloducha <soloducha@gmail.com>
2022-10-23 16:57:49 -0300

    tercer commit, header y footer terminado

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,14 @@ from re import template
 from django.urls import path
 
 from blog.views import (
-    inicio
+    inicio,
+    about
 )
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", inicio, name="inicio"),
+    path("inicio/", inicio, name="inicio"),
+    path("about/", about, name="about"),
+   
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, "inicio.html")
 
+def about(request):
+    return render(request, "about.html")
+
+def blogs(request):
+    return render(request, "blogs.html") #busca todos los blogs y muestra en un a lista
